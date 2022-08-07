39a2c2ad19a99adb223d178dbfb82671c03b2205
M3XCT <108612834+M3XCT@users.noreply.github.com>
2022-08-07 23:06:42 +0300

    Я
     хотел сделать выбор кол во товара как в ленте или озон, но я чё то недопёр как две кнопки сунуть в одну и что бы они появлялись,поэтому я добавил круглую кнопку что бы в дальнейшем выскакивало окно где будет происходить ввод числа которое будет передоваться в текст ячейки товара в рамке

--- flut/lib/pages/page2.dart
+++ flut/lib/pages/page2.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 class page2 extends StatelessWidget {
   const page2({Key? key}) : super(key: key);
 
@@ -21,9 +20,31 @@ class page2 extends StatelessWidget {
                   child:Column(
                     children: [
                       Row(children: [Image(image: AssetImage('assets/138427_1.jpg'),width: 150, height: 150)]),
+                      Row(children: [Text('Шампунь')],),
                       Row(children: [Text('8787мл ООО"лицо"')],),
                       Row(children: [Text('1000'+'р')],),
-                      Row(children: [Text('Шампунь')],)
+                      Row(children: [ElevatedButton(onPressed: (){}, child:Text('Купить'),style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0)))
+                      )
+                      ),
+                        FloatingActionButton.small(
+                          onPressed: (){},
+                          child: Text('шт'),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(15.0),
+                          padding: const EdgeInsets.all(5.0),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black26)
+                          ),
+                          child: Text('i'),
+                        )
+                      ],
+                      ),
+                      Row(children: [],)
+
                     ],
                   )
                   ),
