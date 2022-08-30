35eb383b90a6d9579c7f3ff96def641b6a0e0343
M3XCT <108612834+M3XCT@users.noreply.github.com>
2022-08-30 10:21:10 +0300

    КЛАСС ТОВАРА(ЯЧЕЙКА)И ПОИСК

--- flut/lib/pages/page2.dart
+++ flut/lib/pages/page2.dart
@@ -1,79 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flut/tovar/product.dart';
+import 'package:flut/widgets/search.dart';
 class page2 extends StatelessWidget {
   const page2({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:SafeArea(
-        child:SingleChildScrollView(scrollDirection: Axis.vertical,
+        body:Container(
+          child:
+         SingleChildScrollView(scrollDirection: Axis.vertical,
           child:Column(
-            children: [
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [SearchForm(),
               Row(
+               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Container(
-                      padding: const EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 4,),
-                        borderRadius: BorderRadius.all(Radius.circular(10))
-                      ),
-                  child:Column(
-                    children: [
-                      Row(children: [Image(image: AssetImage('assets/138427_1.jpg'),width: 150, height: 150)]),
-                      Row(children: [Text('Шампунь')],),
-                      Row(children: [Text('8787мл ООО"лицо"')],),
-                      Row(children: [Text('1000'+'р')],),
-                      Row(children: [ElevatedButton(onPressed: (){}, child:Text('Купить'),style: ButtonStyle(
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0)))
-                      )
-                      ),
-
-
-                        Container(
-                          margin: const EdgeInsets.all(15.0),
-                          padding: const EdgeInsets.all(5.0),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black26)
-                          ),
-                          child: Text('i'),
-                        )
-                      ],
-                      ),
-                      Row(children: [],)
-
-                    ],
-                  ),
-
-                Column(
-                  children: [
-            Row(children: [Image(image: AssetImage('assets/138427_1.jpg'),width: 150, height: 150)]),
-        Row(children: [Text('Шампунь')],),
-        Row(children: [Text('8787мл ООО"лицо"')],),
-        Row(children: [Text('1000'+'р')],),
-        Row(children: [ElevatedButton(onPressed: (){}, child:Text('Купить'),style: ButtonStyle(
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0)))
-    )
-    ),
-
-
-    Container(
-    margin: const EdgeInsets.all(15.0),
-    padding: const EdgeInsets.all(5.0),
-    decoration: BoxDecoration(
-    border: Border.all(color: Colors.black26)
-    ),
-    child: Text('i'),
-    )
-                    ]),
-                  ],
-                ),
-
-    )],
+                Product(),
+                  Product(),
+                ],
+              ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Product(),
+              Product(),
+            ],
           ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Product(),
+                  Product(),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Product(),
+                  Product(),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Product(),
+                  Product(),
+                ],
+              ),
+
         ])
         )
 
--- flut/lib/tovar/product.dart
+++ flut/lib/tovar/product.dart
@@ -1,2 +1,44 @@
 import 'package:flutter/material.dart';
 
+class Product extends StatelessWidget {
+  const Product({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+            border: Border.all(width: 4,),
+            borderRadius: BorderRadius.all(Radius.circular(10))
+        ),
+        child:Column(
+          children: [
+            Row(children: [Image(image: AssetImage('assets/138427_1.jpg'),width: 150, height: 150)]),
+            Row(children: [Text('Шампунь')],),
+            Row(children: [Text('8787мл ООО"лицо"')],),
+            Row(children: [Text('1000'+'р')],),
+            Row(children: [ElevatedButton(onPressed: (){}, child:Text('Купить'),style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0)))
+            )
+            ),
+
+
+              Container(
+                margin: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black26)
+                ),
+                child: Text('i'),
+              )
+            ],
+            ),
+
+
+          ],
+        )
+    );
+  }
+}
